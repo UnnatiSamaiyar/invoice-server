ef3db57b2e8427f69b77b3e8e0561077d144a871
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2026-05-25 12:38:42 +0530

    port

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,11 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(4000);
-  console.log('Backend running on http://localhost:4000');
+  const port = Number(process.env.PORT) || 4000;
+
+  await app.listen(port, '127.0.0.1');
+
+  console.log(`Backend running on http://localhost:${port}`);
 }
 
 bootstrap();
