45ff88e47dd342d92e7544e80df05d5f5d180ba8
UnnatiSamaiyar <unnatisamaiyar02@gmail.com>
2026-05-27 16:17:47 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3023',
+    origin: 'https://invoice.thethrottle.in',
     credentials: true,
   });
 
